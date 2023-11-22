5105ce9870f604ab2054b089190c077f52f38f85
PARK JAEHA <parkjaeha97@gmail.com>
2023-11-22 21:02:35 +0900

    04 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>; //구조분해할당
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
+
+const foodLike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    image:
+      'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    image:
+      'https://i.namu.wiki/i/SdI6mHzKiWg-yJEqBpWhaACTth0ZSU_qSjhHCnQp5OxqwVl-PRIzql6bkmp1tm0YaHxjDoTbt-nBtiSE4nhqbQ.webp',
+    rating: 4,
+  },
+  {
+    id: 3,
+    name: 'Bibimbap',
+    image:
+      'https://i.namu.wiki/i/dgjXU86ae29hDSCza-L0GZlFt3T9lRx1Ug9cKtqWSzMzs7Cd0CN2SzyLFEJcHVFviKcxAlIwxcllT9s2sck0RA.jpg',
+    rating: 3.9,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
+      {foodLike.map((dish) => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 export default App;
